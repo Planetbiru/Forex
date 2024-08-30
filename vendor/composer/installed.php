ee53255316a4e80dbad92f3f2e19878aadfee1c0
Kamshory, MT <kamshory@yahoo.com>
2024-08-30 22:56:29 +0700

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'planetbiru/forex',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '92ed0611f7270e9f4cf7c6a6bbdbdebe044bb4b8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'planetbiru/forex' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '92ed0611f7270e9f4cf7c6a6bbdbdebe044bb4b8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
